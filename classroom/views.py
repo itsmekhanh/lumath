c746d3f70fb619dcd54fb54a2ebcbb9e9a29f832
Khanh Luc <itsmekhanh@gmail.com>
2013-08-13 23:20:21 -0700

    added landing page

--- classroom/views.py
+++ classroom/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the classroom index.")
+   return render(request, 'classroom/index.html')
